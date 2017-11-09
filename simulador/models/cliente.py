df5802fc1b7fd5cc5699ae88cf314fc1dc454aa2
Vitor Marques de Miranda <vitor251093@yahoo.com.br>
2017-11-09 21:15:55 -0200

    Cliente agora tem comentários corretos

--- simulador/models/cliente.py
+++ simulador/models/cliente.py
@@ -1,12 +1,12 @@
-// ID
-//
-// Tempo de chegada na fila 1
-// Tempo de chegada no serviço 1
-// Tempo de têrmino do serviço 1
-// Tempo de serviço 1
-//
-// Tempo de chegada na fila 2
-// Tempo de chegada no serviço 2
-// Tempo de têrmino do serviço 2
-// Tempo não-contínuo de serviço 2
-// Tempo decorrido de serviço 2
+# ID
+#
+# Tempo de chegada na fila 1
+# Tempo de chegada no serviço 1
+# Tempo de têrmino do serviço 1
+# Tempo de serviço 1
+#
+# Tempo de chegada na fila 2
+# Tempo de chegada no serviço 2
+# Tempo de têrmino do serviço 2
+# Tempo não-contínuo de serviço 2
+# Tempo decorrido de serviço 2
