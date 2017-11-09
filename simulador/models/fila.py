f15ff059b2356babfbf9901b45fb5afafdaba5cf
Vitor Marques de Miranda <vitor251093@yahoo.com.br>
2017-11-09 21:31:32 -0200

    Adicionados comentários que definirão classe de fila

---
+++ simulador/models/fila.py
@@ -0,0 +1,6 @@
+# ID
+# Array
+#
+# retirarClienteEmAtendimento (retorna cliente e remove do topo da array)
+# adicionarClienteAFila (entra com um cliente)
+# clienteEmAtendimento (retorna o cliente no índice zero (ponteiro))
