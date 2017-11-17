c86970da6a3f85d0a8f630769e9e7662cdd69ac4
Vitor <vitor251093@gmail.com>
2017-11-16 23:01:09 -0200

    timerDeCliente agora retorna o Timer

--- simulador/simulacao.py
+++ simulador/simulacao.py
@@ -17,7 +17,7 @@ class Simulacao(object):
         return self.tempoAtual() - self.__momentoDeInicio
 
     def timerDeCliente(self, funcaoTempo, funcaoExecucao, *args):
-        t = Timer(('cliente.%s' % (funcaoTempo)), 'from __main__ import cliente', funcaoExecucao(*args))
+        return Timer(('cliente.%s' % (funcaoTempo)), 'from __main__ import cliente', funcaoExecucao(*args))
 
     def clienteEntraNaFila(self, cliente):
         print "Cliente %d chegou na fila em: %f" % (cliente.getID(), self.tempoDeSimulacao())
