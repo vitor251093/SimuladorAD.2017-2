98aeab757d726ad587a85f772b8858b6f6379d42
andre-figueiredo <andregus@gmail.com>
2017-11-21 17:52:44 -0200

    Testes no loop

--- simulador/simulacao.py
+++ simulador/simulacao.py
@@ -9,7 +9,7 @@ class Simulacao(object):
         self.__numero_de_clientes = 100
         self.__lambd = 1.0
         self.__tempo = 1000000.0
-        self.__tempo_de_servico = 1.0 # So pra teste
+        self.__tempo_de_servico = 1.0
 
     def run(self):
         t = 0.0
@@ -26,16 +26,21 @@ class Simulacao(object):
             if fila1.numeroDePessoasNaFila() == 0:
                 proxima_chegada = agendador.agendarChegadaFila1(t, self.__lambd)
                 inicio_de_servico = proxima_chegada
-                print "Cliente 0 chega em: %f e inicia servico imediatamente." % (chegada)
+                print "Cliente 0 chega em: %f e inicia servico imediatamente." % (proxima_chegada)
             else:
-                chegada = chegada + agendador.agendarChegadaFila1(t, self.__lambd)
-                inicio_de_servico = max(chegada, fila1.getCliente(-1).getTempoTerminoServico1())
-                print "Cliente %d chega em: %f" % (id_cliente, chegada)
+                proxima_chegada = proxima_chegada + agendador.agendarChegadaFila1(t, self.__lambd)
+                inicio_de_servico = max(proxima_chegada, fila1.getCliente(-1).getTempoTerminoServico1())
 
-            cliente = Cliente(id_cliente, chegada, inicio_de_servico, self.__tempo_de_servico)
+                if (proxima_chegada < fila1.getCliente(-1).getTempoTerminoServico1()):
+                    print "Cliente %d chega na fila em: %f ." % (id_cliente, proxima_chegada)
+                else:
+                    print "Cliente %d estava sendo atendido enquanto Cliente %d chegou em %f" % (fila1.getCliente(-1).getID(), id_cliente, proxima_chegada)
+                print "Cliente %d chega em: %f" % (id_cliente, proxima_chegada)
+
+            cliente = Cliente(id_cliente, proxima_chegada, inicio_de_servico, self.__tempo_de_servico)
             fila1.adicionarClienteAFila(cliente)
             id_cliente+=1
-            t = chegada
+            t = proxima_chegada
 
 
 if __name__ == "__main__":
