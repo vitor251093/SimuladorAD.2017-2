fefd4cd65e28a3793f449b9fd9f81431b61b90e7
Vitor Marques de Miranda <vitor251093@yahoo.com.br>
2017-11-28 22:04:15 -0200

    Adicionada versão alfa da aplicação para plot

---
+++ plot/plot.py
@@ -0,0 +1,21 @@
+import numpy as numpy
+import matplotlib.pyplot as pyplot
+
+class Plot(object):
+
+    def __init__(self):
+        return
+
+    def run(self):
+        # N = numpy.loadtxt('traces.csv', unpack=True, delimiter=',')
+        N = [1,10,2,8,4,6,5,5,5,5]
+        pyplot.plot(N)
+        #pyplot.title('')
+        #pyplot.ylabel('')
+        #pyplot.xlabel('')
+
+        pyplot.show()
+
+
+if __name__ == "__main__":
+    Plot().run()
