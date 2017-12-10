cac0f874f2b7c1890a2d9f0a369d6614c4e19705
Vitor <vitor251093@gmail.com>
2017-12-10 11:13:33 -0200

    Agora o plot.py recebe como argumento o arquivo que deverá ser plotado

--- plot/plot.py
+++ plot/plot.py
@@ -1,15 +1,39 @@
+import random
 import numpy as numpy
 import matplotlib.pyplot as pyplot
+import sys
 
 class Plot(object):
 
     def __init__(self):
-        return
+        self.__colorSeed = 0
+        self.__cores = []
+
+    def numeroAleatorioDe0A255(self):
+        self.__colorSeed += 60
+        random.seed(self.__colorSeed)
+        return int(round(random.random()*255))
+
+    def corParaIndice(self, indice):
+        if len(self.__cores) <= indice + 1:
+            self.__cores.append("#%s%s%s" % (format(self.numeroAleatorioDe0A255(), '02X'),
+                                             format(self.numeroAleatorioDe0A255(), '02X'),
+                                             format(self.numeroAleatorioDe0A255(), '02X')))
+        return self.__cores[indice + 1]
 
     def run(self):
-        N = numpy.loadtxt('vetorEN10k.csv', unpack=True, delimiter=',')
-        #N = [1,10,2,8,4,6,5,5,5,5]
-        pyplot.plot(N)
+        if len(sys.argv) <= 1:
+            print "ERRO: O nome do arquivo .csv que sera plotado deve ser passado como argumento. Ele deve estar no mesmo diretorio do plot.py, ou entao o caminho do .csv inteiro devera ser passado."
+            return
+
+        N = numpy.loadtxt(sys.argv[1], unpack=True, delimiter=',')
+        
+        for indiceVetor in range(len(N[0])-1):
+            valor1 = N[0][indiceVetor]
+            valor2 = N[0][indiceVetor+1]
+            cor    = int(N[1][indiceVetor])
+            pyplot.plot([indiceVetor, indiceVetor+1], [valor1, valor2], self.corParaIndice(cor))
+
         #pyplot.title('')
         #pyplot.ylabel('')
         #pyplot.xlabel('')
